f8c3b9a4c1fb5f658776bf07948b39f274f13fdd
Bidyut Roy <infobt365@gmail.com>
2024-07-28 23:46:38 +0000

    Initial commit

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("simpsons","simpsons")
+        ERC721("Fuck","Fuck")
         Ownable(initialOwner)
     {}
 
